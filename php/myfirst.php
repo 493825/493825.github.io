1767344dc69f32b29bd800caef567e0f56631018
Nikita Pedro <nikita18006@gmail.com>
2019-06-12 11:27:53 +0300

    php added and many other things

---
+++ php/myfirst.php
@@ -0,0 +1,17 @@
+<?php
+
+$a="HELP ME";
+$b="PLEASE";
+print("---------------------\n");
+print ($a.$b);
+print("\n");
+print ($a." ".$b);
+print("\n");
+print ($a.", ".$b."!");
+print("\n");
+print("=====================\n");
+
+
+ 
+?>
+
